666ad6e1a3f541266665690583e92ddd58af4dda
Aoi <ht.so.ik0102@gmail.com>
2021-07-20 23:25:09 +0700

    change the style in upload image

--- src/components/Crowdfunding/UploadImage.tsx
+++ src/components/Crowdfunding/UploadImage.tsx
@@ -1,5 +1,11 @@
 import React, { ChangeEvent, useState, useEffect } from "react";
-import { Typography, Grid, TextField, Button, makeStyles } from "@material-ui/core";
+import {
+  Typography,
+  Grid,
+  TextField,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
 import { DropzoneArea, DropzoneDialogBase } from "material-ui-dropzone";
 import { useDispatch, useSelector } from "react-redux";
 import IconButton from "@material-ui/core/IconButton";
@@ -8,6 +14,13 @@ import CloseIcon from "@material-ui/icons/Close";
 const useStyles = makeStyles({
   layout: {
     height: "23rem",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  buttonLayout: {
+    padding: "9rem",
   },
 });
 
@@ -46,40 +59,42 @@ const UploadImage = () => {
         <Typography variant="h6" gutterBottom>
           Show off your catchy photos
         </Typography>
-        <Grid container spacing={3}>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => setOpen(true)}
-          >
-            Add Image
-          </Button>
+        <div className={classes.buttonLayout}>
+          <Grid container spacing={3}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setOpen(true)}
+            >
+              Add Image
+            </Button>
 
-          <DropzoneDialogBase
-            dialogTitle={dialogTitle()}
-            acceptedFiles={["image/*"]}
-            fileObjects={fileObjects}
-            cancelButtonText={"cancel"}
-            submitButtonText={"submit"}
-            maxFileSize={5000000}
-            open={open}
-            onAdd={(newFileObjs: any) => {
-              console.log("onAdd", newFileObjs);
-              setFileObjects([].concat(fileObjects, newFileObjs));
-            }}
-            onDelete={(deleteFileObj) => {
-              console.log("onDelete", deleteFileObj);
-            }}
-            onClose={() => setOpen(false)}
-            onSave={() => {
-              console.log("onSave", fileObjects);
-              handleImage(fileObjects);
-              setOpen(false);
-            }}
-            showPreviews={true}
-            showFileNamesInPreview={true}
-          />
-        </Grid>
+            <DropzoneDialogBase
+              dialogTitle={dialogTitle()}
+              acceptedFiles={["image/*"]}
+              fileObjects={fileObjects}
+              cancelButtonText={"cancel"}
+              submitButtonText={"submit"}
+              maxFileSize={5000000}
+              open={open}
+              onAdd={(newFileObjs: any) => {
+                console.log("onAdd", newFileObjs);
+                setFileObjects([].concat(fileObjects, newFileObjs));
+              }}
+              onDelete={(deleteFileObj) => {
+                console.log("onDelete", deleteFileObj);
+              }}
+              onClose={() => setOpen(false)}
+              onSave={() => {
+                console.log("onSave", fileObjects);
+                handleImage(fileObjects);
+                setOpen(false);
+              }}
+              showPreviews={true}
+              showFileNamesInPreview={true}
+            />
+          </Grid>
+        </div>
       </React.Fragment>
     </div>
   );
